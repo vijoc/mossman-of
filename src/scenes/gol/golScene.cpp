5aa979c807b7e1868c915148f9114b37bc9e8db2
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2016-12-08 10:39:50 +0200

    add missing files

---
+++ src/scenes/gol/golScene.cpp
@@ -0,0 +1,14 @@
+#include "golScene.hpp"
+
+GolScene::GolScene() {
+	return;
+}
+
+void GolScene::draw() {
+	ofSetColor(255, 0, 0);
+	ofDrawRectangle(10, 10, 50, 50);
+}
+
+void GolScene::update() {
+
+}
---
+++ src/scenes/gol/golScene.hpp
@@ -0,0 +1,9 @@
+#include "ofMain.h"
+#include "scene.hpp"
+
+class GolScene : public Scene {
+public:
+	GolScene();
+	void draw();
+	void update();
+};
