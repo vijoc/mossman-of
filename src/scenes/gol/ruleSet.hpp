ec327d1d21fd9d3b65ba80ad90cef44e36fa31ac
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2016-12-25 15:58:17 +0200

    better initialization

--- src/scenes/gol/ruleSet.hpp
+++ src/scenes/gol/ruleSet.hpp
@@ -3,14 +3,14 @@
 
 class RuleSet {
 public:
-	RuleSet() {;}
+	RuleSet(): birthCounts({ 3 }), survivalCounts({ 2, 3 }) {;}
 	RuleSet(std::vector<int> birthCounts, std::vector<int> survivalCounts): birthCounts(birthCounts), survivalCounts(survivalCounts) {;}
 
 	bool birthRulesContain(int i) { return std::find(birthCounts.begin(), birthCounts.end(), i) != birthCounts.end(); }
 	bool survivalRulesContain(int i) { return std::find(survivalCounts.begin(), survivalCounts.end(), i) != survivalCounts.end(); }
 private:
-	std::vector<int> birthCounts = { 3 };
-	std::vector<int> survivalCounts = { 2, 3 };
+	std::vector<int> birthCounts;
+	std::vector<int> survivalCounts;
 };
 
 #endif /* RULESET_H */
